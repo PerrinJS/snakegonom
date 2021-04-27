f1252282ecc5845ef1b7d655bca7d106b92bc0db
Perrin Smith <perrinjamessmith@gmail.com>
2021-04-27 22:51:03 +0800

    Tile: simplified tile to an enum as currently it is not necisary and complicates things to have it as a class

--- Tile.hpp
+++ Tile.hpp
@@ -1,6 +1,15 @@
 #pragma once
 
+typedef enum
+{
+    SNAKE,
+    PELLET
+} Tile;
+
+//TODO: this is for later refactoring
+/* This is only a class because I eventually want to make it more than just
+ * the adverage snake game *
 class Tile
 {
 
-};
+};*/
