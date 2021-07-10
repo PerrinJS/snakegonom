f49b4b04e279c441ca4f33da63b2096b04461023
Perrin Smith <perrinjamessmith@gmail.com>
2021-07-10 20:28:51 +0800

    GEntity: added Tile to GEntity namespace

--- Tile.hpp
+++ Tile.hpp
@@ -1,15 +1,18 @@
 #pragma once
 
+namespace GEntity
+{
 typedef enum
 {
     SNAKE,
     PELLET
 } Tile;
 
-//TODO: this is for later refactoring
+// TODO: this is for later refactoring
 /* This is only a class because I eventually want to make it more than just
- * the adverage snake game *
+* the adverage snake game *
 class Tile
 {
 
 };*/
+} // namespace GEntity
